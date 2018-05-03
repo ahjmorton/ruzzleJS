3cf90c95b5dcef93e1b4977decf40ed12254a4ff
Andrew Morton <andrew.morton@fundflow.de>
2018-05-03 20:22:33 +0100

    Merge json files into single file

--- src/js/wordlists/index.js
+++ src/js/wordlists/index.js
@@ -1,10 +1,3 @@
 export default function() {
-  return import("./json/wordlist.json").then(wordlist => {
-    return import("./json/markov.json").then(markov => {
-      return {
-        wordlist: wordlist,
-        markov: markov
-      };
-    });
-  });
+  return import("./wordlist.json");
 }
